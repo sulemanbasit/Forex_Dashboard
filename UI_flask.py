88dbccbd4b52ae2b3007cc732d626056449ea8c9
sulemanbasit <83623379+sulemanbasit@users.noreply.github.com>
2024-05-11 00:38:56 -0600

    creating mini blocks for accounts to display properly.

--- UI_flask.py
+++ UI_flask.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+        # Read the CSV file
+    data = pd.read_csv("test.csv")
+
+    # Group data by 'Type' column
+    grouped_data = data.groupby('Type')
+
+    # Convert the grouped data into a dictionary
+    data_dict = {group: group_data.to_dict('records') for group, group_data in grouped_data}
+    
+    return render_template("index.html", data_dict=data_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
