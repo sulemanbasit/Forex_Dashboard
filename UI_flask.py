24b2b3347bcb224cf2231dacc50c59c3598d4b3c
sulemanbasit <83623379+sulemanbasit@users.noreply.github.com>
2024-05-21 22:16:33 -0600

    corrected the last commit by changing the main program instead of the html file.

--- UI_flask.py
+++ UI_flask.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 def read_csv_with_pandas(filename):
     df = pd.read_csv(filename)
+    
+    # Ensure the 'Type' column values are in lowercase
+    if 'Type' in df.columns:
+        df['Type'] = df['Type'].str.lower()
+    
     headers = tuple(df.columns)
     entries = tuple(tuple(row) for row in df.values.tolist())
     return (headers, entries)
@@ -13,7 +18,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 # Example usage:
-    filename = 'API_Fetch_Data/api_web.csv'  # Replace 'data.csv' with your CSV file name
+    filename = 'api_web.csv'  # Replace 'data.csv' with your CSV file name
     headings, data = read_csv_with_pandas(filename)
     return render_template('index2.html', headings=headings,data=data)
 
